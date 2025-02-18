2c56249e100f5383bc5a4537fb4fee4de9dd3396
manishnangare <nangaremanish@gmail.com>
2025-02-17 22:47:01 -0500

    Task 3

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,36 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+     return cart.reduce((total, item) => total + (item.cost * item.quantity), 0).toFixed(2);
   };
 
   const handleContinueShopping = (e) => {
-   
+     onContinueShopping(e);
   };
 
 
 
   const handleIncrement = (item) => {
+    const updatedItem = { ...item, quantity: item.quantity + 1 };
+    dispatch(updateQuantity(updatedItem));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+      const updatedItem = { ...item, quantity: item.quantity - 1 };
+      dispatch(updateQuantity(updatedItem));
+    } else {
+      dispatch(removeItem(item));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+     return (item.cost * item.quantity).toFixed(2);
   };
 
   return (
